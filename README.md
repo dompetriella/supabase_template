a3a4ecfa84e4f0ceb87f9a9cb859f08fbd5603fd
Dominic Petriella <50121548+dompetriella@users.noreply.github.com>
2024-07-09 11:26:30 -0400

    switched over to dart define

--- README.md
+++ README.md
@@ -2,10 +2,11 @@
 
 A template for setting up a supabase flutter app quickly
 
-_requires a "dotenv" file to connect to supabase api_
+_requires a .json file with a baseUrl and apiKey to connect to supabase api_
 
 # Renaming
 
 rename setAppName --targets ios,android,macos,windows,linux --value "newNameHere"
 ( you may need to run "flutter pub global activate rename" first)
+
 # supabase_template
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:supabase_template/app/app.dart';
 
 void main() async {
-  await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
+  String apiKey = const String.fromEnvironment('API_KEY');
+  String baseUrl = const String.fromEnvironment('BASE_URL');
 
-  await Supabase.initialize(
-      url: dotenv.env['BASE_URL'].toString(),
-      anonKey: dotenv.env['API_KEY'].toString());
+  await Supabase.initialize(url: baseUrl, anonKey: apiKey);
   runApp(const ProviderScope(child: App()));
 }
 
